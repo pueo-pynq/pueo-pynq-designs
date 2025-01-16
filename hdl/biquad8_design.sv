edad62a0c46570bceed5f1c95fa0ff518b36c44c
Patrick Allison <allison.122@osu.edu>
2025-01-16 12:09:45 -0500

    make gate delay changeable via parameter

--- hdl/biquad8_design.sv
+++ hdl/biquad8_design.sv
@@ -94,25 +94,27 @@ module biquad8_design(
     reg [127:0] adc0_rereg = {128{1'b0}};
     reg [127:0] adc1_rereg = {128{1'b0}};
     reg adc_gate = 0;
-    wire capture_delay;
-    wire mid_gate_delay;
-    wire gate_delay_done;
+
+    // if we have 2 internal delays we need
+    // 3 signals: input to 0, between 0->1, output from 1
+    // so it's NUM_GATE_DELAYS:0
+    localparam NUM_GATE_DELAYS = 3;
+    wire [NUM_GATE_DELAYS:0] internal_chain;
+    assign internal_chain[0] = capture_i;
+    wire gate_delay_done = internal_chain[NUM_GATE_DELAYS];    
+    generate
+        genvar i;
+        for (i=0;i<NUM_GATE_DELAYS;i=i+1) begin : DLP
+                SRLC32E u_dly(.D(internal_chain[i]),
+                              .CLK(aclk),
+                              .CE(1'b1),
+                              .Q31(internal_chain[i+1]));
+        end
+    endgenerate
+    wire post_gate_delay_0;
     wire post_gate_delay_1;
-    wire post_gate_delay_2;
     wire post_gate_finish;
     reg biquad_reset = 0;
-    SRLC32E u_gate_delay(.D(capture_i),
-                         .CLK(aclk),
-                         .CE(1'b1),
-                         .Q31(capture_delay));
-    SRLC32E u_delay_mid(.D(capture_delay),
-                        .CLK(aclk),
-                        .CE(1'b1),
-                        .Q31(mid_gate_delay));                           
-    SRLC32E u_delay_done(.D(mid_gate_delay),
-                         .CLK(aclk),
-                         .CE(1'b1),
-                         .Q31(gate_delay_done));                                 
         SRLC32E u_postgate_0(.D(gate_delay_done),
                              .CLK(aclk),
                              .CE(1'b1),
